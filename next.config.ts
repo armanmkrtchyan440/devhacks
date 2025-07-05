0c354699729336a9cdc1d39032e34f82fd119613
armanmkrtchyan440 <armanmkrtchyan440@gmail.com>
2025-07-05 13:20:59 +0400

    change

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	env: {
-		WEBSOCKET_URL: process.env.WEBSOCKET_URL,
-	},
 }
 
 export default nextConfig
